b1019fb8e50de02588e3b8534ddf18924a336490
Joe Bugajski <yogurtmystery@gmail.com>
2019-02-17 13:26:05 -0600

    dbConfig reads environment dynamically

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -1,6 +1,11 @@
-const knex = require('knex');
+// const knex = require('knex');
 
-const knexConfig = require('../knexfile');
-const dbEnv = process.env.TESTING_DB || 'development';
+// const knexConfig = require('../knexfile');
+// const dbEnv = process.env.TESTING_DB || 'development';
 
-module.exports = knex(knexConfig[dbEnv]);
+// module.exports = knex(knexConfig[dbEnv]);
+
+const dbEngine = process.env.DB || 'development';
+const config = require('../knexfile')[dbEngine];
+
+module.exports = require('knex')(config);
--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,15 @@
 // Update with your config settings.
 
+require('dotenv').config();
+const localPg = {
+  host: 'localhost',
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+};
+
+const dbConnection = process.env.DATABASE_URL || localPg;
+
 module.exports = {
 
   development: {
@@ -17,7 +27,7 @@ module.exports = {
   },
 
   staging: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
       database: 'my_db',
       user:     'username',
@@ -28,23 +38,21 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './data/migrations'
     }
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: dbConnection,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './data/migrations'
     }
   },
 
